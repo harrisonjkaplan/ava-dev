ec4fa781225524a13362635c9d8003f3759c6eec
harrisonjkaplan <86628873+harrisonjkaplan@users.noreply.github.com>
2023-05-05 23:18:40 -0600

    graph updates and tests

--- src/graph.py
+++ src/graph.py
@@ -2,61 +2,50 @@ from coord import Coord
 class Graph: 
 #takes in elevation data and returns a graph of points on easier x and y plots top right is -r, r elevation[0]
 
-    def __init__(self,r,elevations,cf):
-        self.r = r
+    def __init__(self,num_steps,elevations):
+        self.num_steps = num_steps
         self.elevations = elevations
-        self.g = []
-        self.gList = []
+        self.grid = []
+        self.grid_list = []
        
         #Coords to use for bresenham and easier calculation
-        x = 0
-        for i in range(int(self.r*2)-1):
-            yCoord = self.r-1-i
-            newAdd = []
-                #print("didthis")
-            for j in range(int(self.r*2)-1):
-                xCoord = j-self.r+1
-                c = Coord(xCoord,-yCoord,elevations[x])
-                x = x +1
-                self.gList.append(c)
-                    #c.print2d()
-                newAdd.append(c)
-            self.g.append(newAdd)
-
-
-    
+        coord_count = 0
+        for i in range(int(self.num_steps*2)-1):
+            y_val = self.num_steps-1-i
+            row = []
+            for j in range(int(self.num_steps*2)-1):
+                x_val = j-self.num_steps+1
+                new_coord = Coord(x_val,y_val,elevations[coord_count])
+                coord_count = coord_count + 1
+                self.grid_list.append(new_coord)
+                row.append(new_coord)
+            self.grid.append(row)
 
     def printXandY(self):
-        for i in range(self.r*2-1):
-            for j in range(self.r*2-1):
-                #print("|  ", end = "  ")
-                s = ""
-                s = s + str(self.g[i][j].getX())
-                s = s + ","
-                s = s + str(self.g[i][j].getY())
+        for i in range(self.num_steps*2-1):
+            s = f""
+            for j in range(self.num_steps*2-1):
+                s = s + f"({self.grid[i][j].get_x()},{self.grid[i][j].get_y()})"
+            print(s)
                 
-                #print(s, end = " ")
-
-            #print("")
-
-    def xList(self):
+    def x_list(self):
         xS = []
-        for i in range(self.r*2-1):
-            for j in range(self.r*2-1):
-                xS.append(self.g[i][j].getX())
+        for i in range(self.num_steps*2-1):
+            for j in range(self.num_steps*2-1):
+                xS.append(self.grid[i][j].get_x())
         return xS 
     
-    def yList(self):
+    def y_list(self):
         yS = []
-        for i in range(self.r*2-1):
-            for j in range(self.r*2-1):
-                yS.append(self.g[i][j].getY())
+        for i in range(self.num_steps*2-1):
+            for j in range(self.num_steps*2-1):
+                yS.append(self.grid[i][j].get_y())
         return yS 
 
-    def zList(self):
+    def z_list(self):
         zS = []
-        for i in range(self.r*2-1):
-            for j in range(self.r*2-1):
-                zS.append(self.g[i][j].getZ())
+        for i in range(self.num_steps*2-1):
+            for j in range(self.num_steps*2-1):
+                zS.append(self.grid[i][j].get_z())
         return zS 
     
--- src/test_unit.py
+++ src/test_unit.py
@@ -4,20 +4,21 @@ from coord_field import CoordField
 from graph import Graph
 from haversine import haversine, Unit
 from helpers import get_fake_elevations
+import random
 
 
 class TestAVA(unittest.TestCase):
     def test_coord(self):
         c = Coord(1,2,3)
-        self.assertEqual(c.getX(),1)
-        self.assertEqual(c.getY(),2)
-        self.assertEqual(c.getZ(),3)
-        self.assertEqual(c.toString(),"(1, 2, 3)")
+        self.assertEqual(c.get_x(),1)
+        self.assertEqual(c.get_y(),2)
+        self.assertEqual(c.get_z(),3)
+        self.assertEqual(c.to_string(),"(1, 2, 3)")
 
     def test_CoordField(self):
         step_size = .1
         cf = CoordField(75,75,1,step_size)
-        cf.fillField()
+        cf.fill_field()
 
         self.assertEqual(cf.num_steps,10)
 
@@ -33,6 +34,25 @@ class TestAVA(unittest.TestCase):
         #Within a 9 centimeters of error
         self.assertEqual(dist1_diff<.0009,True)
         self.assertEqual(dist2_diff<.0009,True)
+    
+    def test_graph(self):
+        num_steps = 5
+        random_elevations = [random.randint(0, 10) for _ in range((num_steps*2-1)**2)]
+        g = Graph(num_steps,random_elevations)
+
+        self.assertEqual(len(g.grid),num_steps*2-1)
+        self.assertEqual(len(g.grid[0]),num_steps*2-1)
+
+        self.assertEqual(g.grid[0][0].get_z(),random_elevations[0])
+        self.assertEqual(g.grid[-1][-1].get_z(),random_elevations[-1])
+
+        fake_x_list = [-4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4, -4, -3, -2, -1, 0, 1, 2, 3, 4]
+        fake_y_list = [4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2, -2, -2, -2, -2, -2, -2, -2, -2, -3, -3, -3, -3, -3, -3, -3, -3, -3, -4, -4, -4, -4, -4, -4, -4, -4, -4]
+        self.assertEqual(g.x_list(),fake_x_list)
+        self.assertEqual(g.y_list(),fake_y_list)
+        self.assertEqual(g.z_list(),random_elevations)
+
+
 
     def test_helpers(self):
         elevations = get_fake_elevations(10)
